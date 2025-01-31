45ecd2069957f19587734ed9860e8adb7f3a3c71
soyum2222 <soyum2222@163.com>
2025-01-31 15:39:38 +0800

    Update nui.nvim.lua

--- nvim/lua/plugins/nui.nvim.lua
+++ nvim/lua/plugins/nui.nvim.lua
@@ -1,4 +1,39 @@
 return {
 	"MunifTanjim/nui.nvim",
-	lazy = true
+	lazy = true,
+
+	config = function()
+		require("noice").setup({
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 5,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+					},
+				},
+			},
+		})
+	end,
 }
